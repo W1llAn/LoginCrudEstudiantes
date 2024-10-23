7e2794944221e3cce5999260b3a7e7d702fa7a80
W1llAn <wchimborazo2840@uta.edu.ec>
2024-10-23 15:56:17 -0500

    Cambio en los modelos para el funcionamiento con  APIRest
    Se cambió los modelos para que funcionen como clases con métodos públicos y llamarlos a la API

--- models/editar.php
+++ models/editar.php
@@ -1,22 +1,26 @@
 <?php
-    include 'conexion.php';
-    $conn = new conexion();
-    $con = $conn -> conectar();
+class CrudE{
+    public static function editarEstudiante(){
+        include 'conexion.php';
+        parse_str(file_get_contents("php://input"), $_PUT);
+        $conn = new conexion();
+        $con = $conn -> conectar();
 
-    $cedula = $_POST['cedula'];
-    $nombre = $_POST['estNombre'];
-    $apellido = $_POST['estApellido'];
-    $direccion = $_POST['estDireccion'];
-    $telefono = $_POST['estTelefono'];
-    $curId = $_POST['curId'];
+        $cedula = $_GET['cedula'];
+        $nombre = $_PUT['estNombre'];
+        $apellido = $_PUT['estApellido'];
+        $direccion = $_PUT['estDireccion'];
+        $telefono = $_PUT['estTelefono'];
+        $curId = $_PUT['curId'];
 
-    $sqlEditar = "UPDATE estudiantes SET estNombre = '$nombre', estApellido = '$apellido', estDireccion = '$direccion', estTelefono = '$telefono', curId = $curId WHERE estCedula = '$cedula'";
+        $sqlEditar = "UPDATE estudiantes SET estNombre = '$nombre', estApellido = '$apellido', estDireccion = '$direccion', estTelefono = '$telefono', curId = $curId WHERE estCedula = '$cedula'";
 
-    if($con -> query($sqlEditar)== TRUE){
-        echo json_encode('Se edito el estudiante');
+        if($con -> query($sqlEditar)== TRUE){
+            echo json_encode('Se edito el estudiante');
+        }
+        else{
+            echo json_encode('Fallo al borrar el estudiante'.$sqlEditar.$mysqli->error);
+        }
     }
-    else{
-        echo json_encode('Fallo al borrar el estudiante'.$sqlEditar.$mysqli->error);
-    }
-
+}
 ?>
