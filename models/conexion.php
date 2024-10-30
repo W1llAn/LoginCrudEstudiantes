b547049368df3ecff7d76c548d6b445b5de1169b
W1llAn <wchimborazo2840@uta.edu.ec>
2024-10-30 14:09:51 -0500

    Cambio en la conexión

--- models/conexion.php
+++ models/conexion.php
@@ -8,21 +8,30 @@ class conexion{
             //Se agregan los datos de la base de datos
             /* UID(userid) = nombre de usuario = administradorSOA
             pwd = contraseña = Soa_estudiantes159@ */
-            $connectionInfo = array("UID" => "administradorSOA", "pwd" => "{Soa_estudiantes159@}", "Database" => "estudiantes-quinto", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+            //$connectionInfo = array("UID" => "administradorSOA", "pwd" => "{Soa_estudiantes159@}", "Database" => "estudiantes-quinto", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
             //El nombre del servidor se crea aparte
-            $serverName = "tcp:crud-estudiantes.database.windows.net,1433";
+            //$serverName = "tcp:crud-estudiantes.database.windows.net,1433";
             //Se envia establece la conexión usando sqlsrv_connect
-            $conn = sqlsrv_connect($serverName, $connectionInfo);
-            if(!$conn){
+            //$conn = sqlsrv_connect($serverName, $connectionInfo);
+            /* if(!$conn){
                 die('error en la conexion'.mysqli_connect_error());
             }
             else{
                 //En caso de ser exitoso se retorna el objeto
                 return $conn;
-            }
+            } */
+            $server = "tcp:crud-estudiantes.database.windows.net,1433";
+            $database = "estudiantes-quinto";
+            $username = "administradorSOA";
+            $password = "Soa_estudiantes159@";
+            
+        $conn = new PDO("odbc:Driver={ODBC Driver 17 for SQL Server};Server=$server;Database=$database;Encrypt=yes;", $username, $password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
         } catch (\Throwable $th) {
             echo $th;
         }
+        return $conn;
         
     }
 }
